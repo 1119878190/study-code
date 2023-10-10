6b6b390f8815ac717890619a7ed68017ba9d6515
liuxu <liuxu@zqykj.com>
2023-10-10 17:09:47 +0800

    feat 导出word

---
+++ study-toWord/src/main/java/com.study.toWord/ExportWordByTemplate.java
@@ -0,0 +1,18 @@
+package com.study.toWord;
+
+import java.io.FileNotFoundException;
+import java.util.Random;
+
+/**
+ * @author lx
+ * @date 2023/04/28
+ */
+public class ExportWordByTemplate {
+
+    public static void main(String[] args) throws FileNotFoundException {
+
+
+        Random random = new Random();
+        System.out.println(random.nextInt(999999));
+    }
+}
